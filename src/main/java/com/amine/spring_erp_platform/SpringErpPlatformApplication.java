82f6fdf884cd742bc2f4f22356a84161700d69c6
AMINE7119 <amine71198@gmail.com>
2026-04-14 15:41:58 +0100

    chore: complete project structure and maven configuration

---
+++ src/main/java/com/amine/spring_erp_platform/SpringErpPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.amine.spring_erp_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErpPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErpPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amine/spring_erp_platform/SpringErpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.spring_erp_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErpPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
