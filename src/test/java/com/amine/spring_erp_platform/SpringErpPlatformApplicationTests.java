20d804efa04f748c543dcd1e215c733ccd32603c
AMINE7119 <amine71198@gmail.com>
2026-04-14 15:41:58 +0100

    chore: complete project structure and maven configuration

---
+++ src/test/java/com/amine/spring_erp_platform/SpringErpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.***REMOVED***.spring_erp_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErpPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
