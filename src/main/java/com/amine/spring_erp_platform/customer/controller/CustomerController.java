11ac6ae35c902928279a588829bce49410108f8a
AMINE7119 <amine71198@gmail.com>
2026-04-16 09:26:26 +0100

    feat: add complete CRUD for Customer module with Flyway migration

---
+++ src/main/java/com/amine/spring_erp_platform/customer/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package com.amine.spring_erp_platform.customer.controller;
+
+import com.amine.spring_erp_platform.customer.entity.Customer;
+import com.amine.spring_erp_platform.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    // 1. Ajouter un nouveau client (POST)
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.createCustomer(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    // 2. Récupérer la liste de tous les clients (GET)
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    // 3. Récupérer un client par son ID (GET)
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+        return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+    // 4. Mettre à jour un client (PUT)
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.updateCustomer(id, customer));
+    }
+
+    // 5. Supprimer un client (DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build(); // Renvoie un code 204 No Content (succès sans corps de réponse)
+    }
+}
---
+++ src/main/resources/db/migration/V2__create_customers_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customers (
+                           id BIGSERIAL PRIMARY KEY,
+                           first_name VARCHAR(255) NOT NULL,
+                           last_name VARCHAR(255) NOT NULL,
+                           email VARCHAR(255) UNIQUE NOT NULL,
+                           phone VARCHAR(255),
+                           address VARCHAR(255)
+);
